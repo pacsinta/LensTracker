4d6fc61c8c064fc76087b7f5bad260a8c9946825
Patrik Csikós <patrik.csikos@proton.me>
2024-06-05 23:24:33 +0200

    Modify SettingsUi

--- app/src/main/java/com/cstcompany/lenstracker/ui/SettingsUi.kt
+++ app/src/main/java/com/cstcompany/lenstracker/ui/SettingsUi.kt
@@ -1,9 +1,14 @@
 package com.cstcompany.lenstracker.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.Icon
@@ -13,13 +18,21 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cstcompany.lenstracker.ui.theme.LensTrackerTheme
+import kotlin.math.exp
 
 @Preview
 @Composable
@@ -55,8 +68,37 @@ fun SettingsUi(
                     }
                 }
             ) { padding ->
-                
+                Column(
+                    modifier = Modifier
+                        .padding(top = 55.dp)
+                        .fillMaxSize()
+                        .padding(bottom = 50.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    ExpirationSetter()
+                    ExpirationSetter()
+                }
             }
         }
+    }
+}
+
+@Composable
+fun ExpirationSetter() {
+    Row(
+        Modifier.padding(bottom = 10.dp, top = 10.dp)
+    ) {
+        Text("Right expiration date: ", fontSize = 30.sp)
+
+        var expirationDate by remember{ mutableIntStateOf(30) }
+        TextField(
+            value = expirationDate.toString(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = {
+                expirationDate = it.toInt()
+            },
+            modifier = Modifier.width(54.dp)
+        )
     }
 }
